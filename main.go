e80268d84f7bc8506c6b018e8454aed266535a1d
Avi Banerjee <avibanerjee2000@gmail.com>
2025-01-22 04:20:38 +0530

    new routes added

--- main.go
+++ main.go
@@ -7,16 +7,19 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
-	// mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/assets/", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets/logo.png"))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
-	// })
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
-	err := server.ListenAndServe() // nil means use DefaultServeMux
+	err := server.ListenAndServe() 
     if err != nil {
         fmt.Println("Error starting server:", err)
     }
