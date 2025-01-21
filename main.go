8968885b0a62cb77099c72f0702c13a7856cdef3
Avi Banerjee <avibanerjee2000@gmail.com>
2025-01-21 21:38:44 +0530

    added logo to assets

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
 	// mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
 
 	// })
