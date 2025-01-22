5d43547df9cdf43a199217b1f9c7b020dd580b1c
Avi Banerjee <avibanerjee2000@gmail.com>
2025-01-22 20:44:18 +0000

    api route added

--- main.go
+++ main.go
@@ -3,16 +3,51 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// func (cfg *apiConfig) displayServerHits() http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits.Load())
+// 	})
+// }
+
+// func (cfg *apiConfig) resetServerHitCount() http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits.Swap(0))
+// 	})
+// }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.Handle("/assets/", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets/logo.png"))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
+	apiCfg := &apiConfig{}
+	rootHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(rootHandler))
+	mux.Handle("/assets/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("./assets/"))))
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("GET /api/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileServerHits.Load())))
+	})
+	mux.HandleFunc("POST /api/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileServerHits.Swap(0)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", 0)))
 	})
 
 	server := &http.Server{
